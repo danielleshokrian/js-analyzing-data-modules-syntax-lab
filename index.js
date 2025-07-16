16c6ec4f92c797bab2460ca8a3cc91789a941256
Danielle <danielle.shokrian@aol.com>
2025-07-16 15:05:05 -0400

    added functions to combine user info and add merge date

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+require ('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const userArray of args) {
+   
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  }
+  const today = new Date();
+    combinedObject.merge_date = today.toString("M/d/yyyy");
+    
+    return combinedObject;
+}
+
+module.exports = {
+  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+};
 
 
 module.exports = {
